7c9220a1b2b482f0378bf4e7eaf0557ec9d13fbd
DAKExDUCK <arsengabdulin228@gmail.com>
2022-10-06 01:53:47 +0600

    fix

--- functions/moodle.py
+++ functions/moodle.py
@@ -59,7 +59,7 @@ async def send(chat_id, text):
 async def get_cookies(user_id, BARCODE, PASSWD):
     await set_arsenic_log_level()
     import os
-    service = services.Chromedriver()
+    service = services.Chromedriver(binary='/usr/bin/chromedriver')
     service.log_file = os.devnull
     browser = browsers.Chrome()
     browser.capabilities = {
