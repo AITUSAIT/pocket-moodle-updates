9b840c481f133bce25a3d8c027b6c24e94e1c76e
DAKExDUCK <arsengabdulin228@gmail.com>
2022-10-11 14:46:56 +0600

    fix new user register

--- functions/moodle.py
+++ functions/moodle.py
@@ -91,7 +91,7 @@ async def get_cookies(user_id, BARCODE, PASSWD):
                 error = await session.get_element('div[id=usernameError]')
                 if await error.is_displayed():
                     if not await aioredis.check_if_msg(user_id):
-                        await send(user_id, 'Invalid login or password\n/register_moodle to fix')
+                        await send(user_id, 'Invalid login or password\n/register\_moodle to fix')
                     return {}, False, 'Invalid Login (barcode)'
             except:
                 pass
