fa1401f4765faad4d803d64157a6378940f2fc7e
DAKExDUCK <arsengabdulin228@gmail.com>
2022-10-20 09:11:45 +0600

    add server

---
+++ server/module.py
@@ -0,0 +1,14 @@
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+
+@routes.get('/')
+async def root(request):
+    return web.Response(text="Ok")
+
+
+def start_server():
+    app = web.Application()
+    app.add_routes(routes)
+    web.run_app(app, access_log=None)
